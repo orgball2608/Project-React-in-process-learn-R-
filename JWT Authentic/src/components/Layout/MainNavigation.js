37d3635e60dd0630eda24726857e934d27549a62
20120557 <81631701+orgball2608@users.noreply.github.com>
2022-10-27 21:20:34 +0700

    change password

--- JWT Authentic/src/components/Layout/MainNavigation.js
+++ JWT Authentic/src/components/Layout/MainNavigation.js
@@ -7,6 +7,10 @@ import classes from "./MainNavigation.module.css";
 
 const MainNavigation = () => {
   const AuthCtx = useContext(AuthContext);
+
+  const LogoutHandler = () => {
+    AuthCtx.logout();
+  };
   return (
     <header className={classes.header}>
       <Link to="/">
@@ -25,7 +29,7 @@ const MainNavigation = () => {
                 <Link to="/profile">Profile</Link>
               </li>
               <li>
-                <button>Logout</button>
+                <button onClick={LogoutHandler}>Logout</button>
               </li>
             </Fragment>
           )}
